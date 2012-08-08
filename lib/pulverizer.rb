41c46a8fec74c04cb1013c99bd4ed80f1510c2fc
Thomas Henderson <tom@mathpunk.net>
2012-08-07 21:53:26 -0700

    Metaprogramming for journal, magic, and pages.

--- lib/pulverizer.rb
+++ lib/pulverizer.rb
@@ -1,75 +1,16 @@
-# Textons created by opening up String. Because monkeys are rad. 
+# Textons created by opening up String. Why not? Many reasons. 
 
 class String
 
-  # With more metaprogramming and less repetition, it might look like this:
-  #
-#def double_bracket(pattern)
-  # What, who am I? : self == ?
-  # create return array
-  # find the pattern
-  # ...nongreedily? 
-  # append the group to the return array
-  # re-iterate on what's left in the string
-#end  
+  rubble = {magic: /{{(.*?)}}/m, journal: /\(\((.*?)\)\)/m, pages: /\[\[(.*?)\]\]/m }
 
-  @@magic_pattern = /{{(.*?)}}/m
-  @@page_pattern = /[[(.*)]]/m
-  @@journal_pattern = /\(\((.*)\)\)/m
-
-  #@attributes = Struct.new
-
-  def double_bracket(name, pattern)
-    # pattern = @@#{name}_pattern     # except not wrong
-    # textons = []
-    # self =~ pattern
-    # textons << $1
-    # # make new string that lacks the first group
-    # # call doublebracket on it
-    # @attributes << {#{name} : textons }
-  end
-
-  def magic
-    self =~ @@magic_pattern
-    [$1]
-  end
-
-  def pages
-    self =~ @@page_pattern
-    [$1]
-  end
-
-  def journal
-    self =~ @@journal_pattern
-    [$1]
-  end
-
-
-end
-
-
-
-=begin
-
-= Double Bracket Patterns =
-So anyway, back to double bracket patterns. Consider this:
-
-<<"primitive".defn("A primitive is a tiniest bit of learning. For example, the
-objects for the magic typewriter are complex: textons, databases, and the like.
-But Strings and Kernels are something that you can do things with. They are
-primitives.")>>
-
-The String "primitive" has been asked to do "defn" to "A primitive is a...". If
-there is no such function defined, then "defn" is a property of the texton
-entitled "primitive", and its value is "A primitive is a...".
-
-class String
-  def method_missing
-    if any textons have the name (self)
-      then add methodname as a property of the texton self
-    else
+  rubble.each_entry do |cantrip, incantation|
+    define_method("invoke_#{cantrip}".to_sym) do 
+      textons = []
+      self.scan(incantation) do |words|
+        textons += words
+      end
+    textons
     end
   end
 end
-=end
-
--- test/test_pulverizer.rb
+++ test/test_pulverizer.rb
@@ -4,19 +4,19 @@ require_relative '../lib/pulverizer.rb'
 
 class TestPulverizer < Test::Unit::TestCase
 
-  def test_magic
-    assert_equal ["please"], "The magic word is {{please}}.".magic
-    assert_equal ["please", "thank you"], "The magic words are {{please}} and {{thank you}}.".magic
+  def test_invoke_magic
+    assert_equal ["please"], "The magic word is {{please}}.".invoke_magic
+    assert_equal ["please", "thank you"], "The magic words are {{please}} and {{thank you}}.".invoke_magic
   end
 
-  def test_journal
-    assert_equal ["thought"], "Hmmm... I had a ((thought)).".journal
-    assert_equal ["thoughts", "feelings"], "I'm having ((thoughts)) and ((feelings)).".journal
+  def test_invoke_journal
+    assert_equal ["thought"], "Hmmm... I had a ((thought)).".invoke_journal
+    assert_equal ["thoughts", "feelings"], "I'm having ((thoughts)) and ((feelings)).".invoke_journal
   end
 
-  def test_pages
-    assert_equal ["page"], "This [[page]] intentionally left blank.".pages
-    assert_equal ["documents", "pages"], "Read these [[documents]] and [[pages]].".pages 
+  def test_invoke_pages
+    assert_equal ["page"], "This [[page]] intentionally left blank.".invoke_pages
+    assert_equal ["documents", "pages"], "Read these [[documents]] and [[pages]].".invoke_pages 
   end
 
 end
