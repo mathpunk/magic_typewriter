638eac9d87930af942e9b9428fe903ccf3cd743a
Thomas Henderson <tom@mathpunk.net>
2012-08-13 14:17:22 -0700

    A bad solution is found!
    In attempting to refactor these methods so that they are ordinary string methods
    but with sigilization, I may have possibly effed up their ability to handle
    blocks. But, my tests are passing! And I don't quite get blocks!
    
    Point being, if you can think of a way to flatten the array without a block, be
    my guest. I couldn't find it, due to a method_missing for 'flatten' being called
    on my Texton -- I have no idea where, I thought I was calling it on an array
    returned by String#split.

--- lib/texton.rb
+++ lib/texton.rb
@@ -10,13 +10,36 @@ class Texton < String
                ideas: /^\*{3,}$/, 
                beats: /\*{2,}/ 
              }                                                                        
+
   def method_missing (method_name, &block)
     message = method_name.to_s.split('_')
     method = message.shift
     # This method expects only one sigil word; it could be improved with an
     # iterator 
     sigil = message.shift
-    self.send(method.to_sym, @@sigils[sigil.to_sym], &block)
+
+    # Things that don't work:
+    # --------------------------------------------------- 
+    # self.send(method.to_sym, @@sigils[sigil.to_sym], &block)
+    # It is the closest to working, but it gives an array of arrays instead of
+    # an array.
+    # 
+    # self.send(method.to_sym, @@sigils[sigil.to_sym], &block).flatten
+    # --> flatten is somehow a method missing, in THIS class. Which I guess
+    # means that I'm not sending the message to an array -- which means,
+    # perhaps, I'm misunderstanding a binding/sending/ordering problem. 
+    #
+    # results = []
+    # self.send(method.to_sym, @@sigils[sigil.to_sym], &block) do |found|
+    #   results += found
+    # end
+
+    results = []
+    self.send(method.to_sym, @@sigils[sigil.to_sym]) do |found|
+      results += found
+    end
+    results
+
   end
 
 end
