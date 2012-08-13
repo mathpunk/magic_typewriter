c520ce095951b7428467c7c6e3a800c5f4b92344
Thomas Henderson <tom@mathpunk.net>
2012-08-13 16:14:26 -0700

    The incorrectly written split_paragraphs test is passing!
    The test passes; however, something weird is going on if you run the in-line
    tests.

--- lib/texton.rb
+++ lib/texton.rb
@@ -12,19 +12,18 @@ class Texton < String
              }                                                                        
 
   def method_missing (method_name, &block)
-    puts "Using method missing on \"#{method_name}\""
+    # puts "Using method missing on \"#{method_name}\""
     message = method_name.to_s.split('_')
     method = message.shift
-    # This method expects only one sigil word; it could be improved with an
-    # iterator 
     sigil = message.shift
+    # puts "Split missing method into \"#{method}\", \"#{sigil}\""
 
     # scans are working, splits aren't -- I'm intercepting split calls to run
     # a variant algorithm. 
 
     results = []
     if method == "split"
-      self.split_variant method, message
+      self.split_variant method, sigil
     else
       self.send(method.to_sym, @@sigils[sigil.to_sym]) do |found|
         results += found
@@ -33,19 +32,28 @@ class Texton < String
     end
   end
 
-  def split_variant method, message
-    puts "Yes, the split variant has been called"
-    puts "the method is \"#{method}\" and the message is \"#{message}\""
-    # Things that do not work
-    # self.send(method.to_sym, @@sigils[sigil.to_sym]) 
+  def split_variant method, sigil
+    # puts "Calling split variant:\n\n\t method: \"#{method}\", sigil: \"#{sigil}\""
+    pattern = @@sigils[sigil.to_sym]
+    # puts "The pattern is #{pattern}"
+    self.send(method.to_sym, pattern).collect {|x| x.strip}
   end
-
-
 end
 
 text = Texton.new("This is a thing **that has a #tag in it **but also [[a page of some kind]]. 
                   
                   And then, a paragraph.")
+
+puts "scan pages:"
 puts text.scan_pages
+
+puts "\nscan tags:"
 puts text.scan_tags
-puts text.split_paragraphs
+
+# This test isn't written right.
+puts "\nscan paragraphs:"
+text.split_paragraphs.each_with_index { |graf, index| puts "paragraph #{index+1}: #{graf}"}
+
+# This test may not be written right.
+puts "\nscan beats:"
+text.split_beats.each_with_index { |graf, index| puts "beat #{index+1}: #{graf}"}
