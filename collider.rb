cc2f50ed23aa3492b74c75b9708566a64916b87a
Thomas Henderson <tom@mathpunk.net>
2012-08-13 14:17:22 -0700

    A bad solution is found!
    In attempting to refactor these methods so that they are ordinary string methods
    but with sigilization, I may have possibly effed up their ability to handle
    blocks. But, my tests are passing! And I don't quite get blocks!
    
    Point being, if you can think of a way to flatten the array without a block, be
    my guest. I couldn't find it, due to a method_missing for 'flatten' being called
    on my Texton -- I have no idea where, I thought I was calling it on an array
    returned by String#split.

--- collider.rb
+++
@@ -1,8 +0,0 @@
-module Config
-  HOST = "wry.23q.org"
-  PORT = 27017
-  COLLECTION = "snow"
-end
-
-module Mongoing
-end
--- test/test_texton.rb
+++ test/test_texton.rb
@@ -1,13 +1,22 @@
 require 'test/unit'
 require 'redgreen'
 require_relative '../lib/texton.rb'
+
 BASE = "/home/thomas/lab/magic_typewriter/"
 
 class TestTexton < Test::Unit::TestCase
   def setup
     @texton = Texton.new(File.open(BASE+"test/texton.txt").read())
-    @plain_texton = "Nothing fancy in here."
+    @plain_texton = Texton.new("Nothing fancy in here.")
+    @paragraph_texton = Texton.new("This is the first paragraph.
+
+This is the second.")
+    @torn_texton = Texton.new("Man this is fun.\n---\nI'm so alone...")
+    @dramatic_texton = Texton.new("I have** wait for it** an idea...\n***\n\nYeah, it's gone.")
   end
+
+  # REFACTOR IDEA: Note that you could do a bunch of "define_methods" on these
+  # guys, for each kv-pair in your sigils dictionary. It might even work. 
 
   def test_scan_magic
     assert_equal ["please", "thank you"], @texton.scan_magic
@@ -16,24 +25,34 @@ class TestTexton < Test::Unit::TestCase
 
   def test_scan_journal
     assert_equal ["thoughts", "feelings"], @texton.scan_journal
-    assert_equal ([] or nil), @plain_texton.scan_magic
+    assert_equal ([] or nil), @plain_texton.scan_journal
   end
 
   def test_scan_pages
     assert_equal ["documents", "pages"], @texton.scan_pages 
-    assert_equal ([] or nil), @plain_texton.scan_magic
-  end
-
-  def test_split_paragraphs
-    text = Texton.new("This is the first paragraph.
-
-This is the second.")
-    assert_equal ["This is the first paragraph." ,"This is the second."], text.split_paragraphs
+    assert_equal ([] or nil), @plain_texton.scan_pages
   end
 
   def test_scan_tags
     text = Texton.new("This has #hashtags in it, such as #foo and #bar.")
     assert_equal ["hashtags", "foo", "bar"], text.scan_tags
+  end
+
+  def test_split_paragraphs
+    assert_equal ["This is the first paragraph." ,"This is the second."], @paragraph_texton.split_paragraphs
+  end
+
+  def test_split_tears
+    assert_equal ["Man this is fun.", "I'm so alone..."], @torn_texton.split_tears
+    # test what happens when you give it a block
+  end
+
+  def test_split_ideas
+    assert_equal  ["I have** wait for it** an idea...", "Yeah, it's gone."] , @dramatic_texton.split_ideas
+  end
+
+  def test_split_beats
+    @dramatic_texton = ["I have", "wait for it", "an idea...", "***\n\nYeah, it's gone."], @dramatic_texton.split_beats
   end
 
 end
