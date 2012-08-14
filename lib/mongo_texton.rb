e98a341ef4c979ea318d335b5304e6e73efdc779
Thomas Henderson <tom@mathpunk.net>
2012-08-13 18:43:11 -0700

    Messed around with database classes without breaking too much.

---
+++ lib/mongo_texton.rb
@@ -0,0 +1,52 @@
+require 'mongo'
+require 'mongo_mapper'
+require_relative 'texton'
+
+# =================================================== 
+module Configuration
+  HOST = "wry.23q.org"
+  PORT = 27017
+  DATABASE = "testing"
+  COLLECTION = "textons"
+  CORPUS_DIR = "/home/thomas/lab/magic_typewriter/corpus"
+end
+# =================================================== 
+
+# Textons and Textinos?
+# Documents and Connectors?
+# What's the pattern here?
+
+class Corpus
+  # get an array of files from the CORPUS
+  # every file's filename is a title
+  # every file's text is text
+end
+
+class Document
+  include MongoMapper::Document
+  
+  key :title, String
+  key :fulltext, String
+  key :tags, Array
+
+  # title = use the filename
+  # text = read the file
+  # tags = an array of tags
+end 
+
+class Mapper
+  def initialize
+    @conn = Mongo::Connection.new(Configuration::HOST, Configuration::PORT)
+    @db   = @conn[Configuration::DATABASE]
+    @coll = @db[Configuration::COLLECTION]
+  end
+
+  def add_record(thing)
+    @coll.insert(thing)
+  end
+
+  def all_records
+    puts "There are #{@coll.count} records. Here they are:"
+    @coll.find.each { |doc| puts doc.inspect }
+  end
+end
