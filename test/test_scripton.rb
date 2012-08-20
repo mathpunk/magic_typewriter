64dd2abb18b36f2b2bcb42ca568f680c884a68d6
Thomas Henderson <tom@mathpunk.net>
2012-08-19 18:15:34 -0700

    Cleanup that I'll commit to master. Minor scripton tests.

--- test/test_scripton.rb
+++ test/test_scripton.rb
@@ -1,14 +1,24 @@
 require 'test/unit'
 require 'redgreen'
 require_relative '../lib/texton.rb'
+require_relative '../lib/scripton.rb'
 
 BASE = "/home/thomas/lab/magic_typewriter/"
 
-class TestTexton < Test::Unit::TestCase
+class TestScripton < Test::Unit::TestCase
   def setup
+    @scripton = Scripton.new([Texton.new("this is"), Texton.new("an array"), Texton.new("of textons")])
   end
 
-  def test_split_beats
+=begin
+  def test_edit
+    assert_respond_to(@scripton, :edit)
+    assert_equal "this is an array of textons", @scripton.edit
+  end
+=end
+
+  def test_query
+    assert_respond_to(@scripton, :query)
   end
 
 end
